9e62651c66ec50b77f98d66332eb18606530a20d
Bernice Esiemot <66827220+brnce@users.noreply.github.com>
2022-10-11 14:08:09 +0100

    Hello World in NetBeans

---
+++ HelloWorld/src/main/java/bernice/helloworld/HelloWorld.java
@@ -0,0 +1,12 @@
+package bernice.helloworld;
+
+/**
+ *
+ * @author berniceesiemot
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
